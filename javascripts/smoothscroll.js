7366d4ecbb7a7be2b0eb2f65512e6f7eb236a0a3
DavidCWebs <david@carawebs.com>
2014-05-31 17:07:03 +0100

    Light background

--- javascripts/smoothscroll.js
+++ javascripts/smoothscroll.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         var target = this.hash,
             $target = $(target);
         $('html, body').stop().animate({
-            'scrollTop': $target.offset().top -150
+            'scrollTop': $target.offset().top -180
         }, 900, 'swing', function () {
             window.location.hash = target;
         });
